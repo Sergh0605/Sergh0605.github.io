03694796b95c299564de1cbe739f9f1d511f839d
Sergh0605 <52658864+Sergh0605@users.noreply.github.com>
2025-12-10 21:49:06 +0300

    Refactor media provider to add menu item and update component

--- myprovider.js
+++ myprovider.js
@@ -1,49 +1,36 @@
 (() => {
 
-    Lampa.Settings.add({
-        title: 'Медиатека',
-        group: 'Мои плагины',
-        name: 'media_provider',
-        type: 'static',
-        value: 'Моя медиатека',
-        onChange: () => {}
-    });
-
-    function request(url) {
-        return fetch(url).then(r => r.json());
+    function addMenuItem() {
+        Lampa.Menu.add({
+            id: 'my_media',
+            title: 'Моя видеотека',
+            icon: 'folder',
+            action: () => {
+                Lampa.Activity.push({
+                    url: '',
+                    title: 'Моя видеотека',
+                    component: 'my_media_main',
+                    page: 1
+                });
+            }
+        });
     }
 
-    Lampa.Listener.follow('app', event => {
+    // ждём, пока загрузится приложение
+    Lampa.Listener.follow('app', (event) => {
         if (event.type === 'ready') {
-
-            Lampa.Activity.push({
-                id: 'my_media',
-                title: 'Моя видеотека',
-                icon: 'folder',
-                component: 'my_media_main'
-            });
-
+            addMenuItem();
         }
     });
 
+    // регистрируем компонент
     Lampa.Component.add('my_media_main', {
-
         render() {
-            let frame = $('<div class="my-media-frame"><div class="scroll"></div></div>');
-            let scroll = frame.find('.scroll');
-
-            request('http://192.168.1.100:3000/shows')
-                .then(list => {
-                    list.forEach(name => {
-                        scroll.append(`<div class="card">${name}</div>`);
-                    });
-                });
-
-            return frame;
+            let html = document.createElement('div');
+            html.innerHTML = `<div style="padding:20px;color:white;font-size:40px;">Моя видеотека работает</div>`;
+            return $(html);
         },
-
         destroy() {}
-
     });
 
 })();
